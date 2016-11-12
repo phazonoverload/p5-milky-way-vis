533fb339b54f901b77f6c73887b690cd1b7eedfb
Kevin Lewis <kevin@jellybabi.es>
2016-11-12 16:51:20 +0000

    Colors

--- data.js
+++ data.js
@@ -5,63 +5,63 @@ var planetData = {
       distance: 57.9, 
       period: 88,
       diameter: 4879,
-      color: "tomato"
+      color: "#AA7C41"
     },
     {
       name: "Venus",
       distance: 108.2,
       period: 224.7,
       diameter: 12104,
-      color: "#009688"
+      color: "#AE4F11"
     },
     {
       name: "Earth",
       distance: 149.6,
       period: 365.26,
       diameter: 12756,
-      color: "skyblue"
+      color: "#5B7881"
     },
     {
       name: "Mars",
       distance: 227.9,
       period: 687,
       diameter: 6794,
-      color: "white"
+      color: "#CD7339"
     },
     {
       name: "Jupiter",
       distance: 778.6,
       period: 4331,
       diameter: 142984,
-      color: "white"
+      color: "#486A81"
     },
     {
       name: "Saturn",
       distance: 1433.5,
       period: 10747,
       diameter: 120536,
-      color: "white"
+      color: "#E9D2AC"
     },
     {
       name: "Uranus",
       distance: 2872.5,
       period: 30589,
       diameter: 51118,
-      color: "white"
+      color: "#6DC6EC"
     },
     {
       name: "Neptune",
       distance: 4495.1,
       period: 59800,
       diameter: 49528,
-      color: "white"
+      color: "#3F89D7"
     },
     {
       name: "Pluto",
       distance: 5870,
       period: 90588,
       diameter: 2390,
-      color: "darkgrey"
+      color: "#A9B2B8"
     }
   ]
 };
--- sketch.js
+++ sketch.js
@@ -5,71 +5,71 @@
 var planets = [];
 var sunSize = 100;
 var maxSize,
-    maxDistance,
-    maxSpeed,
-    simSpeed = 1;
+  maxDistance,
+  maxSpeed,
+  simSpeed = 1;
 
 function drawSun() {
   fill("#EBC62C");
   stroke("white");
   strokeWeight(2);
-  ellipse(width/2, height/2, sunSize);
+  ellipse(width / 2, height / 2, sunSize);
 }
 
 function Planet(name, color, size, distance, speed) {
   this.name = name;
   this.color = color;
   this.size = size;
-  this.sizeInSitu = map(size, 0, maxSize, 0, width/15);
+  this.sizeInSitu = map(size, 0, maxSize, 0, width / 15);
   this.distance = distance;
-  this.distanceInSitu = map(distance, 0, maxDistance, 0, width/2.25) + (sunSize / 2);
+  this.distanceInSitu = map(distance, 0, maxDistance, 0, width / 2.25) + (sunSize / 2);
   this.speed = (360 / speed) * simSpeed / 10;
   this.theta = 0;
   this.posX = 0;
   this.posY = 0;
-  
+
   this.trajectory = function() {
     stroke(this.color);
     strokeWeight(2);
     noFill();
-    ellipse(0, 0, this.distanceInSitu*2);
+    ellipse(0, 0, this.distanceInSitu * 2);
   }
-  
+
   this.drawPlanet = function() {
     fill(color);
     stroke("#2C354A");
     strokeWeight(2);
-    
+
     ellipseMode(CENTER);
     this.posX = this.distanceInSitu * cos(this.theta);
     this.posY = this.distanceInSitu * sin(this.theta);
     ellipse(this.posX, this.posY, this.sizeInSitu, this.sizeInSitu);
     this.theta += this.speed;
   }
-  
+
   this.display = function() {
-    
+
   }
 }
 
 function findMaxValues() {
   // Find planet with biggest size
   var planetSizes = [];
-  for(var i=0; i<planetData.milkyway.length; i++) {
+  for (var i = 0; i < planetData.milkyway.length; i++) {
     planetSizes.push(planetData.milkyway[i].diameter);
   }
   maxSize = Math.max(...planetSizes);
-  
+
   // Find planet with biggest distance
   var planetDistances = [];
-  for(var i=0; i<planetData.milkyway.length; i++) {
+  for (var i = 0; i < planetData.milkyway.length; i++) {
     planetDistances.push(planetData.milkyway[i].distance);
   }
   maxDistance = Math.max(...planetDistances);
-  
+
   // Find planet with biggest period
   var planetSpeeds = [];
-  for(var i=0; i<planetData.milkyway.length; i++) {
+  for (var i = 0; i < planetData.milkyway.length; i++) {
     planetSpeeds.push(planetData.milkyway[i].period);
   }
   maxSpeed = Math.max(...planetSpeeds);
@@ -78,7 +78,7 @@ function findMaxValues() {
 function setup() {
   createCanvas(2000, 2000);
   findMaxValues();
-  for(var i=0; i<planetData.milkyway.length; i++) {
+  for (var i = 0; i < planetData.milkyway.length; i++) {
     var p = planetData.milkyway[i];
     planets[i] = new Planet(p.name, p.color, p.diameter, p.distance, p.period);
   }
@@ -87,8 +87,8 @@ function setup() {
 function draw() {
   background("#2C354A");
   drawSun();
-  translate(width/2, height/2);
-  for(var i=0; i<planets.length; i++) {
+  translate(width / 2, height / 2);
+  for (var i = 0; i < planets.length; i++) {
     planets[i].trajectory();
     planets[i].drawPlanet();
     planets[i].display();
