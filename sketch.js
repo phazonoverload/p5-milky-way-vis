5f7b70f68f408fee5930c794110724d7a2dc4da4
Kevin Lewis <kevin@jellybabi.es>
2016-12-19 17:06:35 +0000

    Started to make functional

--- sketch.js
+++ sketch.js
@@ -3,43 +3,139 @@
 ///////////////////////////////////////////////////////////////////
 
 var planets = [],
-  planetAngles = [],
-  sunSize = 100,
-  maxSize,
-  maxDistance,
-  maxSpeed,
-  simSpeed = 1,
-  slider,
-  viewAnglesButton,
-  infoAngles;
+    planetAngles = [],
+    sunSize = 100;
+  
+var CalculatePlanetValues = {
+  size: 0,
+  distance: 0,
+  speed: 0,
 
-function drawSlider() {
-  slider = createSlider(0, 100, 10);
-  slider.position(30, 30);
-  slider.style("width", "400px");
+  calculateSize: function() {
+    var planetSizes = [];
+    for (var i = 0; i < ControlZoom.numberVisible; i++) {
+      planetSizes.push(planetData.milkyway[i].diameter);
+    }
+    CalculatePlanetValues.size = planetSizes.reduce(function(a, b) {
+      if(a > b) {
+        return a;
+      } else {
+        return b;
+      }
+    }, 0);
+  },
+
+  calculateDistance: function() {
+    var planetDistances = [];
+    for (var i = 0; i < ControlZoom.numberVisible; i++) {
+      planetDistances.push(planetData.milkyway[i].distance);
+    }
+    CalculatePlanetValues.distance = planetDistances.reduce(function(a, b) {
+      if(a > b) {
+        return a;
+      } else {
+        return b;
+      }
+    }, 0);
+  },
+
+  calculateAll: function() {
+    CalculatePlanetValues.calculateDistance();
+    CalculatePlanetValues.calculateSize();
+  }
 }
 
-function drawAnglesButton() {
-  viewAnglesButton = createButton("Click to toggle angles");
-  viewAnglesButton.position(38, 150);
-  viewAnglesButton.mousePressed(function() {
-    infoAngles = !infoAngles;
-  });
+var ControlSpeed = {
+  input: "",
+  value: 1,
+  createInput: function() {
+    ControlSpeed.input = createSlider(0, 100, 10);
+    ControlSpeed.input.position(30, 30);
+    ControlSpeed.input.style("width", "400px");
+  },
+  change: function() {
+    var val = ControlSpeed.input.value();
+    var displayText = val + "% Speed";
+    push();
+    textSize(30);
+    noStroke();
+    fill("#D4D4D4");
+    text(displayText, 475, 60);
+    pop();
+    ControlSpeed.value = val / 10;
+  }
 }
-function speedChange() {
-  var val = slider.value();
-  var displayText = "Current speed is " + val + "%";
-  push();
-  textSize(40);
-  noStroke();
-  fill("#D4D4D4");
-  text(displayText, 40, 120);
-  pop();
-  simSpeed = val / 10;
+
+var ControlAngleVisibility = {
+  input: "",
+  value: false,
+  createInput: function() {
+    ControlAngleVisibility.input = createButton("Click to toggle angles");
+    ControlAngleVisibility.input.position(38, 160);
+    ControlAngleVisibility.input.id("angle-toggle");
+  },
+  change: function() {
+    planetAngles = [];
+    for(var i = 0; i < planets.length; i++) {
+      planetAngles.push(planets[i].angle); 
+    }
+    document.getElementById("angle-toggle").onclick = function() {
+      ControlAngleVisibility.value = !ControlAngleVisibility.value;
+    }
+  }
+}
+
+var ControlZoom = {
+  numberVisible: planetData.milkyway.length,
+  inputOut: "",
+  inputIn: "",
+  createLabel: function() {
+    push();
+    textSize(30);
+    noStroke();
+    fill("#D4D4D4");
+    text("Zoom level", 40, 125);
+    pop();
+  },
+  createInputs: function() {
+    ControlZoom.inputOut = createButton("-");
+    ControlZoom.inputOut.position(220, 90);
+    ControlZoom.inputOut.id("zoom-out");
+    ControlZoom.inputIn = createButton("+");
+    ControlZoom.inputIn.position(280, 90);
+    ControlZoom.inputIn.id("zoom-in");
+  },
+  buttonClick: function() {
+
+  },
+  threshold: function() {
+    // Don't let it go below 1, or above the array length 
+  },
+  newPlanetValues: function() {
+
+  },
+  change: function() {
+    // document.getElementById("zoom-out").onclick = function() {
+    //   console.log("Zoom out clicked");
+    //   ControlZoom.numberVisible++;
+    //   CalculatePlanetValues.calculateAll();
+    //   // Call threshold
+    // };
+    document.getElementById("zoom-in").onclick = function() {
+      console.log("Zoom in clicked");
+      ControlZoom.numberVisible--;
+      CalculatePlanetValues.calculateAll();
+      // Call threshold
+      for(var i = 0; i < ControlZoom.numberVisible; i++) {
+        planets[i].size = map(this.sizeOriginal, 0, CalculatePlanetValues.size, 0, width / 20);
+        planets[i].distance = map(this.distanceOriginal, 0, CalculatePlanetValues.distance, 0, width / 2.25) + (sunSize / 2);
+      }
+    };
+  }
 }
 
 function drawSun() {
-  fill("white");
+  fill("#FECD52");
   stroke("white");
   strokeWeight(2);
   ellipse(width / 2, height / 2, sunSize);
@@ -48,28 +144,27 @@ function drawSun() {
 function Planet(name, color, size, distance, speed) {
   this.name = name;
   this.color = color;
-  this.size = size;
-  this.sizeInSitu = map(size, 0, maxSize, 0, width / 20);
-  this.distance = distance;
-  this.distanceInSitu = map(distance, 0, maxDistance, 0, width / 2.25) + (sunSize / 2);
-  this.speed = 0;
+  this.sizeOriginal = size;
+  this.size = map(size, 0, CalculatePlanetValues.size, 0, width / 20);
+  this.distanceOriginal = distance;
+  this.distance = map(distance, 0, CalculatePlanetValues.distance, 0, width / 2.25) + (sunSize / 2);
+  this.speed = speed;
   this.theta = 0;
   this.angle = 0;
   this.posX = 0;
   this.posY = 0;
-  this.years = 0;
 
-  this.trajectory = function() {
+  this.drawTrajectory = function() {
     stroke(this.color);
     strokeWeight(2);
     noFill();
-    ellipse(0, 0, this.distanceInSitu * 2);
+    ellipse(0, 0, this.distance * 2);
   }
 
-  this.movePlanet = function() {
-    this.posX = this.distanceInSitu * cos(this.theta);
-    this.posY = this.distanceInSitu * sin(this.theta);
-    this.speed = (360 / speed) * simSpeed;
+  this.calculatePlanetPos = function() {
+    this.posX = this.distance * cos(this.theta);
+    this.posY = this.distance * sin(this.theta);
+    this.speed = (360 / speed) * ControlSpeed.value;
     this.theta += this.speed;
   }
 
@@ -78,68 +173,39 @@ function Planet(name, color, size, distance, speed) {
     stroke("#2C354A");
     strokeWeight(2);
     ellipseMode(CENTER);
-    ellipse(this.posX, this.posY, this.sizeInSitu, this.sizeInSitu);
+    ellipse(this.posX, this.posY, this.size, this.size);
   }
 
-  this.drawInfo = function() {
+  this.drawAngle = function() {
     this.angle = parseInt(atan2(-this.posX, -this.posY));
     fill("white");
-    if(infoAngles) {
+    if(ControlAngleVisibility.value) {
       stroke(this.color);
       line(0, 0, this.posX, this.posY); 
       noStroke();
-      text(this.angle + "°", this.posX + this.sizeInSitu / 2, this.posY + this.sizeInSitu / 2);
+      text(this.angle + "°", this.posX + this.size / 2, this.posY + this.size / 2);
     } 
   }
   
   this.display = function() {
-    this.trajectory();
-    this.movePlanet();
+    this.drawTrajectory();
+    this.calculatePlanetPos();
     this.drawPlanet();
-    this.drawInfo();
-  }
-}
-
-function findMaxValues() {
-  // Find planet with biggest size
-  var planetSizes = [];
-  for (var i = 0; i < planetData.milkyway.length; i++) {
-    planetSizes.push(planetData.milkyway[i].diameter);
-  }
-  maxSize = Math.max(...planetSizes);
-
-  // Find planet with biggest distance
-  var planetDistances = [];
-  for (var j = 0; j < planetData.milkyway.length; j++) {
-    planetDistances.push(planetData.milkyway[j].distance);
-  }
-  maxDistance = Math.max(...planetDistances);
-
-  // Find planet with biggest period
-  var planetSpeeds = [];
-  for (var k = 0; k < planetData.milkyway.length; k++) {
-    planetSpeeds.push(planetData.milkyway[k].period);
-  }
-  maxSpeed = Math.max(...planetSpeeds);
-}
-
-function createAngleArr() {
-  planetAngles = [];
-  for(var i = 0; i < planets.length; i++) {
-    planetAngles.push(planets[i].angle); 
+    this.drawAngle();
   }
 }
 
 function setup() {
   createCanvas(2000, 2000);
   angleMode(DEGREES);
-  findMaxValues();
+  CalculatePlanetValues.calculateAll();
   for (var i = 0; i < planetData.milkyway.length; i++) {
     var p = planetData.milkyway[i];
     planets[i] = new Planet(p.name, p.color, p.diameter, p.distance, p.period);
   }
-  drawSlider();
-  drawAnglesButton();
+  ControlSpeed.createInput();
+  ControlAngleVisibility.createInput();
+  ControlZoom.createInputs();
 }
 
 function draw() {
@@ -151,6 +217,8 @@ function draw() {
     planets[i].display();
   }
   pop();
-  createAngleArr();
-  speedChange();
+  ControlSpeed.change();
+  ControlAngleVisibility.change();
+  ControlZoom.createLabel();
+  ControlZoom.change();
 }
